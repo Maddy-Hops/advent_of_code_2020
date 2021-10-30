eb424b3d4465cced8c80207015f44fc65cf71b37
maddy-hops <antondal8@gmail.com>
2021-10-30 16:04:01 +0700

    finish day10

--- src/day10/mod.rs
+++ src/day10/mod.rs
@@ -1,24 +1,73 @@
 use std::time::Instant;
 use std::fs;
+use std::collections::HashMap;
 
 fn generate() -> Vec<i32>{
 	let contents = fs::read_to_string("input/day10.txt").expect("Failed to read the input file");
-	contents.lines()
-	.map(|f| f.parse::<i32>().unwrap()).collect()
+	let mut input: Vec<i32> = contents.lines()
+	.map(|f| f.parse::<i32>().unwrap()).collect();
+	input.sort();
+	// let's add the 0 and the max_adapter + 3 links to our list
+	input.reverse();
+	input.push(0);
+	input.reverse();
+	input.push(input[input.len()-1]+3);
+	input
 	
 }
 
-
+// since the requirements state we must find the chain including **all** adapters
+// we're going to walk through the sorted list and just record the differences between elements
 fn part1(input: &[i32]) -> i32 {
-	0
+	let mut map = HashMap::new();
+	for i in 0..input.len()-1 {
+		let count = map.entry(input[i+1]-input[i]).or_insert(0);
+		*count += 1;
+	}
+	println!("{:?}",&map);
+	map.get(&1).unwrap() * map.get(&3).unwrap()
 }
 
 
 
-fn part2(input: &[i32]) -> i32 {
-	
-	0
+fn part2(input: &[i32]) -> u128 {
+	// finding all the combinations of connecting your device
+	// first we need to find a 3-wide gap, that means there's only one valid way to connect two groups of combinations
+	// we can then calculate the combinations for each group in-between 3-wide gaps separately and multiply them together to get the final answer
+	let mut mult = 1;
+	let mut prev_start = 0;
+	let mut all_slices = vec![];
+	for i in 0..input.len()-1 {
+		if input[i+1] - input[i] == 3  {
+			let slice = &input[prev_start..i+1];
+			all_slices.push(slice.into_iter().map(|&f| f).collect::<Vec<i32>>());
+			prev_start = i+1;
+		}
+	}
+	// each resulting slice will consist of elements separated by one and 
+	// the number of possible paths it has will be following a fibonacci-like
+	// sequence with 3 previous numbers considered, starting with `0,1,1`
+	// like so: 
+	// series = [0,1,1,2,4,7,13,24,44]
+	// n's (zero indexed) element in the series will be the number of combinations in this particular slice
+	// we go through all the slices, discard anything that's below 3 in length (because there's only 1 way to go there and
+	// there's no point in multiplying by 1 a bunch of times) 
+	// then we search for the longest slice in our list of slices, and generate the sequence up to that element
+	// then we simply go over each slice, look at their length and multply our result by series[len(slice)]
+	all_slices.retain(|f|f.len()>=3);
+	let mut longest_slice = 0;
+	all_slices.iter().for_each(|f|if f.len() > longest_slice {longest_slice = f.len()});
+	let mut series = vec![0,1,1];
+	for i in 2..=longest_slice {
+		series.push(series[i-2]+series[i-1]+series[i])
+	}
+	all_slices.iter().for_each(|f|mult *= series[f.len()]);
+	mult
 }
+
+
+
+
 
 pub fn run() {
 	let now = Instant::now();
