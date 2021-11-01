26058cd031aaef0af4ecce49ae94412957debc15
maddy-hops <antondal8@gmail.com>
2021-11-01 13:27:11 +0700

    finish day11 part2

--- src/day11/mod.rs
+++ src/day11/mod.rs
@@ -95,7 +95,60 @@ fn check_neighbours(field: &Vec<Vec<Seat>>, i: usize, j: usize) -> (usize, Seat)
 }
 
 fn part2(input: Vec<Vec<Seat>>) -> i32 {
-	0
+	let mut field_before = input;
+	let mut field_new = field_before.clone();
+	loop {
+		let mut has_changed = false;
+		for i in 1..field_before.len() {
+			for j in 1..field_before[0].len() {
+				match check_neighbours_in_line(&field_before, i, j) {
+					(counter, Seat::Emp) if counter == 0 => {
+						field_new[i][j] = Seat::Occ;
+						has_changed = true;
+					}
+					(counter, Seat::Occ) if counter >= 5 => {
+						field_new[i][j] = Seat::Emp;
+						has_changed = true;
+					}
+					_ => (),
+				}
+			}
+		}
+		field_before = field_new.clone();
+		if !has_changed {
+			break;
+		}
+	}
+	let field_new: Vec<Seat> = field_new.into_iter().flat_map(|f| f).collect();
+	let mut counter = 0;
+	field_new.into_iter().for_each(|f| {
+		if f == Seat::Occ {
+			counter += 1
+		}
+	});
+	counter
+}
+fn check_neighbours_in_line(field: &Vec<Vec<Seat>>, i: usize, j: usize) -> (usize, Seat) {
+	let mut counter = 0;
+
+	for offset in ARR {
+		let (mut loop_i, mut loop_j) = (i as i32, j as i32);
+		while loop_i >= 1
+			&& loop_j >= 1
+			&& (loop_i as usize) < field.len() - 1
+			&& (loop_j as usize) < field[0].len() - 1
+		{
+			loop_i += offset.0;
+			loop_j += offset.1;
+			if field[loop_i as usize][loop_j as usize] == Seat::Occ {
+				counter += 1;
+				break;
+			} else if field[loop_i as usize][loop_j as usize] == Seat::Emp {
+				break;
+			}
+		}
+	}
+	(counter, field[i][j].clone())
 }
 
 pub fn run() {
