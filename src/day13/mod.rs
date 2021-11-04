5b16c1b3b337909a14e4493a1a5d22a3749788b2
maddy-hops <antondal8@gmail.com>
2021-11-04 16:08:06 +0700

    gave up on day13 part2

--- src/day13/mod.rs
+++ src/day13/mod.rs
@@ -1,24 +1,39 @@
+use inline_python::{python, Context};
 use std::fs;
 use std::time::Instant;
-
-fn generate() -> Schedule {
+fn generate() -> (Schedule, Vec<ScheduleEntry>) {
 	let contents = fs::read_to_string("input/day13.txt").expect("Failed to read the input file");
 	let mut iter = contents.lines();
 	let timestamp_arrival = iter.next().unwrap().parse().unwrap();
 	let mut bus_list = vec![];
+	let mut bus_list2 = vec![];
 	iter.next().unwrap().split(",").for_each(|f| {
 		if f != "x" {
-			bus_list.push(f.parse::<i32>().unwrap());
+			let val = f.parse::<i32>().unwrap();
+			bus_list.push(val);
+			bus_list2.push(ScheduleEntry::Bus(val as u128));
+		} else {
+			bus_list2.push(ScheduleEntry::Emp);
 		}
 	});
-	Schedule {
-		timestamp_arrival,
-		bus_list,
-	}
+
+	(
+		Schedule {
+			timestamp_arrival,
+			bus_list,
+		},
+		bus_list2,
+	)
 }
 struct Schedule {
 	timestamp_arrival: i32,
 	bus_list: Vec<i32>,
+}
+
+#[derive(Debug)]
+enum ScheduleEntry {
+	Bus(u128),
+	Emp,
 }
 
 fn part1(input: &Schedule) -> i32 {
@@ -34,7 +49,7 @@ fn part1(input: &Schedule) -> i32 {
 	(zipped_timestamp_bus_vec[0].0 - input.timestamp_arrival) * zipped_timestamp_bus_vec[0].1
 }
 
-fn part2(input: &Schedule) -> i32 {
+fn part2(input: &[ScheduleEntry]) -> u128 {
 	0
 }
 
@@ -48,13 +63,13 @@ pub fn run() {
 	);
 
 	let now = Instant::now();
-	println!("\tPart 1 result: {}", part1(&input));
+	println!("\tPart 1 result: {}", part1(&input.0));
 	println!(
 		"\t\tTime to complete part 1: {} μs",
 		now.elapsed().as_micros()
 	);
 	let now = Instant::now();
-	println!("\tPart 2 result: {:?}", part2(&input));
+	println!("\tPart 2 result: {:?}", part2(&input.1));
 	println!(
 		"\t\tTime to complete part 2: {} μs",
 		now.elapsed().as_micros()
